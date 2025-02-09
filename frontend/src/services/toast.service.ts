7f31cbefa58acbf7ab3f49d772f2cd4e30efccc0
Даниил <danilka.volkov.02@mail.ru>
2025-02-09 16:35:19 +0300

    frontend: добавить ToastService

---
+++ frontend/src/services/toast.service.ts
@@ -0,0 +1,46 @@
+export class ToastService {
+    public constructor(private instance: any) {}
+
+    showMessage(
+        message: string,
+        title: string,
+        severity:
+            | "error"
+            | "success"
+            | "secondary"
+            | "info"
+            | "contrast"
+            | "warn",
+    ): void {
+        this.instance.add({
+            severity: severity,
+            summary: title,
+            detail: message,
+            life: 4000,
+        });
+    }
+
+    showError(message: string, title: string = "Ошибка"): void {
+        this.showMessage(message, title, "error");
+    }
+
+    showSuccess(message: string, title: string = "Успех"): void {
+        this.showMessage(message, title, "success");
+    }
+
+    showSecondary(message: string, title: string = "Информация"): void {
+        this.showMessage(message, title, "secondary");
+    }
+
+    showInfo(message: string, title: string = "Информация"): void {
+        this.showMessage(message, title, "info");
+    }
+
+    showContrast(message: string, title: string = "Информация"): void {
+        this.showMessage(message, title, "contrast");
+    }
+
+    showWarn(message: string, title: string = "Предупреждение"): void {
+        this.showMessage(message, title, "warn");
+    }
+}
