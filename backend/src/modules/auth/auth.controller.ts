03efe98b1654561fa6fe35b7f49b3f3c784ba07b
Даниил <danilka.volkov.02@mail.ru>
2025-02-19 01:05:00 +0300

    backend: добавить метод /auth/profile для получения профиля

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from '@/modules/auth/auth.service';
 import { SignInRequestDto } from '@/modules/auth/dto/sign-in.request.dto';
 import { SignUpRequestDto } from '@/modules/auth/dto/sign-up.request.dto';
 import { SignInResponseDto } from '@/modules/auth/dto/sign-in.response.dto';
 import { SignUpResponseDto } from '@/modules/auth/dto/sign-up.response.dto';
+import { Authorized } from '@/modules/auth/decorators/authorized.decorator';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
+import { ProfileResponseDto } from '@/modules/auth/dto/profile.response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +33,12 @@ export class AuthController {
     @Body() signUpDto: SignUpRequestDto,
   ): Promise<SignUpResponseDto> {
     return this.authService.signUp(signUpDto);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  public profile(@Authorized() user: User): ProfileResponseDto {
+    return new ProfileResponseDto(user);
   }
 }
