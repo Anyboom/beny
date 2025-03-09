c751c5c7471924756b23176eb88e937fc3cabb11
Даниил <danilka.volkov.02@mail.ru>
2025-03-09 18:18:23 +0300

    backend: добавить для сущности Bet вычисляемые поля Status и Coefficient

--- backend/src/modules/bet/entities/bet.entity.ts
+++ backend/src/modules/bet/entities/bet.entity.ts
@@ -1,4 +1,4 @@
-import { Bet } from '@prisma/client';
+import { Bet, StatusEvent } from '@prisma/client';
 import { Exclude, Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { UserEntity } from '@/modules/user/entities/user.entity';
@@ -33,4 +33,40 @@ export class BetEntity implements Bet {
   @Type(() => EventEntity)
   @Expose()
   events: EventEntity[];
+
+  /**
+   * Общий коэффициент.
+   */
+  @ApiProperty()
+  @Expose()
+  get coefficient(): number {
+    return this.events.reduce((sum, event) => sum * event.coefficient, 1);
+  }
+
+  /**
+   * Общий статус.
+   */
+  @ApiProperty()
+  @Expose()
+  get status(): StatusEvent {
+    if (this.events.some((x) => x.status == StatusEvent.LOSE)) {
+      return StatusEvent.LOSE;
+    }
+
+    if (this.events.some((x) => x.status == StatusEvent.RETURN)) {
+      return StatusEvent.RETURN;
+    }
+
+    if (
+      this.events.some(
+        (x) =>
+          x.status !== StatusEvent.LOSE &&
+          x.status !== StatusEvent.NOT_CALCULATED,
+      )
+    ) {
+      return StatusEvent.WIN;
+    }
+
+    return StatusEvent.NOT_CALCULATED;
+  }
 }
